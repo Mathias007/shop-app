5d463494eb92ca87bf8a02a75d5d6eede2c3f9e7
Mathias007 <mathias.najlepszy@gmail.com>
2021-03-28 20:10:21 +0200

    connect with database using typeorm & mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { BasketModule } from "./basket/basket.module";
 import { ShopModule } from "./shop/shop.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-    imports: [BasketModule, ShopModule],
+    imports: [TypeOrmModule.forRoot(), BasketModule, ShopModule],
     controllers: [AppController],
     providers: [AppService],
 })
